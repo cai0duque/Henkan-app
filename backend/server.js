6c2ced96893c09140e9334ffa3fbac4a5b9c5b7d
Caio Duque <147485273+cai0duque@users.noreply.github.com>
2024-12-13 18:42:03 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: '*', //Modifique para origem privada conforme a utilização
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
 });
